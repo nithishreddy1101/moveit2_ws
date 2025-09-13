cae3eb8591061630b80b5dcd03c01252f4172ae4
nithishreddy1101 <nithishreddy2004@gmail.com>
2025-09-13 12:41:12 +0530

    Updated code with new changes

--- build/yolov8_obb_msgs/yolov8_obb_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nithish/ROS/moveit2_ws/build/yolov8_obb_msgs/yolov8_obb_msgs__py
-# Build directory: /home/nithish/ROS/moveit2_ws/build/yolov8_obb_msgs/yolov8_obb_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
